072079be92fd3d713667d774e2e1b1f11c313565
Tejkin <Thomas1467@hotmail.com>
2024-03-10 02:04:57 +0800

    Added arrow assets

--- VictorianMoneyTracker/VictorianMoneyTracker/App.xaml.cs
+++ VictorianMoneyTracker/VictorianMoneyTracker/App.xaml.cs
@@ -15,7 +15,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-namespace Victorian_Money_Tracker
+namespace VictorianMoneyTracker
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
